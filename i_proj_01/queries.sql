900f4b77ce627034c4fbbc66ad35b393a1f14ed6
p-fisher <ops.patrickfisher@gmail.com>
2022-05-19 19:29:28 -0400

    cont

--- i_proj_01/queries.sql
+++ i_proj_01/queries.sql
@@ -17,6 +17,9 @@ SELECT * FROM likes;
 DELETE FROM likes WHERE id IN (5,6);
 
 DELETE FROM ideas WHERE id BETWEEN 15 AND 18;
+DELETE FROM ideas WHERE id = 18;
+commit;
+
 
 INSERT INTO likes (user_id, idea_id) VALUES (4,15);
 
@@ -27,9 +30,15 @@ SELECT * FROM likes LEFT JOIN users ON likes.user_id = users.id LEFT JOIN ideas 
 SELECT * FROM likes LEFT JOIN users ON likes.user_id = users.id LEFT JOIN ideas ON likes.idea_id = ideas.id LEFT JOIN users AS creator ON ideas.user_id = creator.id WHERE ideas.id = 15;
 
 ## tinkering with count on join queries for like counts
-
 #SELECT COUNT(*) FROM likes WHERE (SELECT * FROM ideas LEFT JOIN users ON ideas.user_id=users.id LEFT JOIN likes ON likes.idea_id = ideas.id);
 
 SELECT * FROM ideas LEFT JOIN users ON ideas.user_id=users.id LEFT JOIN (SELECT user_id, COUNT(*) AS like_count FROM likes) ON likes_count.id = ideas.id;
 
+##more tinkering
+SELECT i.user_id,i.summary,u.f_name,u.l_name,u.alias from ideas AS i JOIN users as u ON i.user_id=u.id;
+
+rollback;
+
 SET FOREIGN_KEY_CHECKS=0;
+DELETE FROM ideas WHERE id = 15;
+SELECT * FROM ideas;
